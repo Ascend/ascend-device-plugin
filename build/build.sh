748bb592a4ffed972017e45d6d14f55fa3bb959e
BianTanggui <tutianyi@huawei.com>
2020-06-29 11:31:31 +0800

    Match-id-f1b78c600de3773b6a540c199cfb7b3a9824394f

--- build/build.sh
+++ build/build.sh
@@ -3,8 +3,10 @@ CUR_DIR=$(dirname $(readlink -f $0))
 TOP_DIR=$(realpath ${CUR_DIR}/..)
 set -x
 dos2unix build_310.sh
-chmod +x build_310.sh
-./${CUR_DIR}/build_310.sh ci
+chmod 550 build_310.sh
+bash -x ${CUR_DIR}/build_310.sh ci
 cd ${TOP_DIR}/output/
 rm -rf *
-tar -zcvf  ascend-device-plugin.tar.gz ../src/* ../go.mod ../ascendplugin.yaml ../Dockerfile
+tar -zcvf  ascend-device-plugin.tar.gz ../build ../output ../src  \
+ ../ascend.yaml ../ascendplugin.yaml ../docker_run.sh ../Dockerfile ../go.mod \
+ ../README.md
