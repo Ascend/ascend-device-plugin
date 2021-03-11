ecf226bf0f4b7bdfd3440f8eb366d52e9b8f480f
lining <lining152@huawei.com>
2021-03-11 10:44:28 +0800

    【问题描述】k8s插件兼容DL 【解决方案】增加ci_build.sh脚本，包装build.sh容器构建脚本 【修改】修改注释日期（文件创建及修改日期）

---
+++ build/ci_build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Perform build k8s-device-plugin
+# Copyright @ Huawei Technologies CO., Ltd. 2021-2021. All rights reserved
+
+set -e
+set -x
+CUR_DIR="$(dirname "$(readlink -f "$0")")"
+TOP_DIR=$(realpath "${CUR_DIR}"/../..)
+
+mkdir -p "$TOP_DIR"/ascend-device-plugin/build
+mkdir -p "$TOP_DIR"/ascend-device-plugin/output
+cd "$TOP_DIR"/ascend-device-plugin/
+
+# 文件格式转换
+dos2unix "$TOP_DIR"/ascend-device-plugin/build/build.sh
+chmod 550 "$TOP_DIR"/ascend-device-plugin/build/*
+cd "$TOP_DIR"/ascend-device-plugin/build/
+
+version="v20.2.0" 
+
+# 修改build.sh文件
+sed -i "s/build_verison=.*/build_verison=\"${version}\"/" "$TOP_DIR"/ascend-device-plugin/build/build.sh
+sed -i "s/docker_images_name=.*/docker_images_name=\"ascend_k8sdeviceplugin:${version}\"/" "$TOP_DIR"/ascend-device-plugin/build/build.sh
+
+sed -i "s/x86/amd64/" "$TOP_DIR"/ascend-device-plugin/build/build.sh
+
+# 修改三个yaml的镜像
+sed -i "s/ascend_k8sdeviceplugin:.*/ascend_k8sdeviceplugin:${version}/" "$TOP_DIR"/ascend-device-plugin/ascendplugin.yaml
+sed -i "s/ascend_k8sdeviceplugin:.*/ascend_k8sdeviceplugin:${version}/" "$TOP_DIR"/ascend-device-plugin/ascendplugin-volcano.yaml
+sed -i "s/ascend_k8sdeviceplugin:.*/ascend_k8sdeviceplugin:${version}/" "$TOP_DIR"/ascend-device-plugin/ascendplugin-310.yaml
+
+# 执行构建
+bash -x "$TOP_DIR"/ascend-device-plugin/build/build.sh dockerimages
+
+# 拷贝出修改后的yaml镜像
+cp "$TOP_DIR"/ascend-device-plugin/ascendplugin.yaml "$TOP_DIR"/ascend-device-plugin/output/ascendplugin-"${version}".yaml
+cp "$TOP_DIR"/ascend-device-plugin/ascendplugin-volcano.yaml "$TOP_DIR"/ascend-device-plugin/output/ascendplugin-volcano-"${version}".yaml
+cp "$TOP_DIR"/ascend-device-plugin/ascendplugin-310.yaml "$TOP_DIR"/ascend-device-plugin/output/ascendplugin-310-"${version}".yaml
