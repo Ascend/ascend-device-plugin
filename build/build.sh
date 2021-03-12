854981e646a9be41726e145542722627c29cd16b
BianTanggui <lining152@huawei.com>
2021-03-12 10:38:21 +0800

    Match-id-0b3c74c58cb8ed5cf6d0868e9a6e5150f1627091

--- build/build.sh
+++ build/build.sh
@@ -38,7 +38,6 @@ function build_plugin() {
     export CGO_ENABLED=1 
     export CGO_CFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv"
     export CGO_CPPFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv" 
-    export CGO_LDFLAGS="-Wl,-z,relro,-z,now -Wl,-z,noexecstack"
     go build -buildmode=pie -ldflags "-X main.BuildName=${output_name} \
             -X main.BuildVersion=${build_version} \
             -buildid none     \
