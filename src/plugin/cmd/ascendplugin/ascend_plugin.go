0d53f065f069408e73fcf7c94ccb2afc6a98ff12
BianTanggui <lining152@huawei.com>
2021-03-18 15:04:22 +0800

    Match-id-51fb989177b772a5222d8087f87000d7540c96d3

--- src/plugin/cmd/ascendplugin/ascend_plugin.go
+++ src/plugin/cmd/ascendplugin/ascend_plugin.go
@@ -74,7 +74,7 @@ func main() {
 
 	neverStop := make(chan struct{})
 	switch *mode {
-	case "ascend310", "pci", "vnpu", "ascend910", "":
+	case "ascend310", "pci", "vnpu", "ascend910", "ascend710", "":
 		logger.Info("ascend device plugin running mode", zap.String("mode", *mode))
 	default:
 		logger.Info("unSupport mode, waiting indefinitely", zap.String("mode", *mode))
