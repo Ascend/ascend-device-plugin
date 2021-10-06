33a39f421d4a7b8c090d35866d0fd151353f9720
BianTanggui <tutianyi@huawei.com>
2021-10-06 15:21:51 +0800

    Match-id-c7b478c2a615c7a992649bb8743153ba801127be

--- src/plugin/pkg/npu/huawei/ascend310.go
+++ src/plugin/pkg/npu/huawei/ascend310.go
@@ -9,6 +9,7 @@ import "C"
 import (
 	"huawei.com/npu-exporter/hwlog"
 	"k8s.io/apimachinery/pkg/util/sets"
+	pluginapi "k8s.io/kubelet/pkg/apis/deviceplugin/v1beta1"
 )
 
 // HwAscend310Manager manages huawei Ascend310 devices.
@@ -31,11 +32,24 @@ func (hnm *HwAscend310Manager) GetMatchingDeviType() string {
 
 // DoWithVolcanoListAndWatch ascend310 affinity scheduling
 func (hnm *HwAscend310Manager) DoWithVolcanoListAndWatch(hps *HwPluginServe, isStateChange bool) {
+	hnm.reloadHealthDevice(isStateChange, hps)
 	usedDevices := sets.NewString()
 	getNodeNpuUsed(&usedDevices, hps)
 	freeDevices := hps.healthDevice.Difference(usedDevices)
 	groupAllocatableDevs := groupDevByPower(freeDevices, hps.devType)
 	if err := hps.kubeInteractor.patchAnnotationOnNode(groupAllocatableDevs, hiAIAscend310Prefix); err != nil {
 		hwlog.Errorf("Ascend310 patch Annotation failed, err: %v", err)
+	}
+}
+
+func (hnm *HwAscend310Manager) reloadHealthDevice(isStateChange bool, hps *HwPluginServe) {
+	if !isStateChange {
+		return
+	}
+	hps.healthDevice = sets.String{}
+	for _, device := range hps.devices {
+		if device.Health == pluginapi.Healthy {
+			hps.healthDevice.Insert(device.ID)
+		}
 	}
 }
