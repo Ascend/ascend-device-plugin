7019d931c5cf962e9cc7d7cc25a33b4a04827495
BianTanggui <lining152@huawei.com>
2022-12-14 20:26:23 +0800

    Match-id-27fd148021aac7da05cb3fd300f8c719c3ad8042

--- pkg/server/manager_test.go
+++ pkg/server/manager_test.go
@@ -8,11 +8,16 @@ import (
 	"reflect"
 	"testing"
 
-	"Ascend-device-plugin/pkg/common"
-	"Ascend-device-plugin/pkg/kubeclient"
 	"github.com/agiledragon/gomonkey/v2"
 	"github.com/smartystreets/goconvey/convey"
 	"huawei.com/npu-exporter/devmanager"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"Ascend-device-plugin/pkg/common"
+	"Ascend-device-plugin/pkg/device"
+	"Ascend-device-plugin/pkg/kubeclient"
 )
 
 // TestTestNewHwDevManager for testTestNewHwDevManager
@@ -43,9 +48,118 @@ func TestStartAllServer(t *testing.T) {
 				return fmt.Errorf("error")
 			})
 		defer mockStart.Reset()
-		defer mockStart.Reset()
 		hdm := NewHwDevManager(&devmanager.DeviceManagerMock{}, &kubeclient.ClientK8s{})
 		res := hdm.startAllServer(&common.FileWatch{})
 		convey.So(res, convey.ShouldBeFalse)
 	})
+}
+
+// TestUpdatePodAnnotation for testUpdatePodAnnotation
+func TestUpdatePodAnnotation(t *testing.T) {
+	node := getMockNode(common.Ascend310P)
+	podDeviceInfo := []PodDeviceInfo {
+		{
+			Pod: getMockPod(),
+			KltDevice: []string{},
+			RealDevice: []string{},
+		},
+		{
+			Pod: getMockPod(),
+			KltDevice: []string{""},
+			RealDevice: []string{""},
+		},
+	}
+	convey.Convey("test updatePodAnnotation", t, func() {
+		convey.Convey("updatePodAnnotation success", func() {
+			mockNode := gomonkey.ApplyMethod(reflect.TypeOf(new(kubeclient.ClientK8s)), "GetNode",
+				func(_ *kubeclient.ClientK8s) (*v1.Node, error) {
+					return node, nil
+				})
+			mockPodDeviceInfo := gomonkey.ApplyMethod(reflect.TypeOf(new(PluginServer)), "GetKltAndRealAllocateDev",
+				func(_ *PluginServer) ([]PodDeviceInfo, error) {
+					return podDeviceInfo, nil
+				})
+			mockManager := gomonkey.ApplyMethod(reflect.TypeOf(new(device.AscendTools)), "AddPodAnnotation",
+				func(_ *device.AscendTools, _ *v1.Pod, _ []string, _ []string, _ string, _ string) error {
+					return nil
+				})
+			defer mockManager.Reset()
+			defer mockNode.Reset()
+			defer mockPodDeviceInfo.Reset()
+			hdm := NewHwDevManager(&devmanager.DeviceManagerMock{}, &kubeclient.ClientK8s{})
+			err := hdm.updatePodAnnotation()
+			convey.So(err, convey.ShouldBeNil)
+		})
+	})
+}
+
+// TestUpdateDevice for testUpdateDevice
+func TestUpdateDevice(t *testing.T) {
+	convey.Convey("test UpdateDevice", t, func() {
+		convey.Convey("UpdateDevice success", func() {
+			mockCheckLabel := gomonkey.ApplyMethod(reflect.TypeOf(new(device.AscendTools)),
+				"CheckDeviceTypeLabel",
+				func(_ *device.AscendTools) error {
+					return nil
+				})
+			mockDestroy := gomonkey.ApplyMethod(reflect.TypeOf(new(PluginServer)), "DestroyNotUsedVNPU",
+				func(_ *PluginServer) error {
+					return nil
+				})
+			defer mockDestroy.Reset()
+			defer mockCheckLabel.Reset()
+			common.ParamOption.PresetVDevice = false
+			hdm := NewHwDevManager(&devmanager.DeviceManagerMock{}, &kubeclient.ClientK8s{})
+			hdm.ServerMap[common.AiCoreResourceName] = NewPluginServer(common.Ascend310P, nil, nil, nil)
+			err := hdm.updateDevice()
+			convey.So(err, convey.ShouldBeNil)
+			common.ParamOption.PresetVDevice = true
+		})
+	})
+}
+
+func getMockPod() v1.Pod {
+	return v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "mindx-dls-npu-1p-default-2p-0",
+			Namespace:   "btg-test",
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{Resources: v1.ResourceRequirements{
+					Limits: v1.ResourceList{
+						common.HuaweiAscend910: resource.Quantity{},
+					},
+				}},
+			},
+		},
+		Status: v1.PodStatus{
+			Reason: "UnexpectedAdmissionError",
+			ContainerStatuses: []v1.ContainerStatus{
+				{State: v1.ContainerState{
+					Waiting: &v1.ContainerStateWaiting{},
+				}},
+			},
+		},
+	}
+}
+
+func getMockNode(ascendType string) *v1.Node {
+	return &v1.Node{
+		Status: v1.NodeStatus{
+			Allocatable: v1.ResourceList{
+				v1.ResourceName(ascendType): resource.Quantity{},
+			},
+			Addresses: getAddresses(),
+		},
+	}
+}
+
+func getAddresses() []v1.NodeAddress {
+	return []v1.NodeAddress{
+		{
+			Type:    v1.NodeHostName,
+			Address: common.DefaultDeviceIP,
+		},
+	}
 }
