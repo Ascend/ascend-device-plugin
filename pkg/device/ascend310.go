b30fa6b855de2e8c0363f448362a10041689ec3a
lining <lining152@huawei.com>
2023-06-01 14:42:19 +0800

    【修改说明 Modification】 device-plugin支持设备虚拟化 【修改人】lining 【评审人】kangfuan

--- pkg/device/ascend310.go
+++ pkg/device/ascend310.go
@@ -73,7 +73,7 @@ func (hnm *HwAscend310Manager) getNPUsByNormalMode(davinCiDev common.DavinCiDev)
 }
 
 func (hnm *HwAscend310Manager) getNPUsByShareMode(davinCiDev common.DavinCiDev) []common.NpuDevice {
-	var shareDevices []common.NpuDevice
+	shareDevices := make([]common.NpuDevice, common.ParamOption.ShareCount)
 	for id := uint(davinCiDev.LogicID) * common.ParamOption.ShareCount; id < uint(davinCiDev.LogicID+1)*
 		common.ParamOption.ShareCount; id++ {
 		deviceName := fmt.Sprintf("%s-%d", hnm.name, id)
